660858a575f2216f58d95bdcad33a01001463b80
Ashish Kumar <ashish.kumar@primathon.in>
2023-11-30 11:26:28 +0530

    initial commit

---
+++ container/hello/index.tsx
@@ -0,0 +1,7 @@
+const Hello = () => {
+    console.log('hello');
+
+    return <div className='text-red-500'>Gellp</div>;
+};
+
+export default Hello;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import FileAndFolder from 'container/fileAndFolder';
+import { folderStructure } from 'container/fileAndFolder/data';
+import Hello from 'container/hello';
+
+const Home = () => (
+  <h1 className='text-red-500 text-7xl'>
+    sb kuch thik h pr frontend walo s deployment nhi krwanaa chahiye tha esa
+    kisi mahapurush n bola h pr hmne whatsappp pr pdha tha: Ashish kumar
+  </h1>
+);
+
+export default Home;
